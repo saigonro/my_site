9acdb56141297ed8df0c0d0d27a5139270591769
Dan Dumitrache <vietnamezul@gmail.com>
2017-09-16 06:31:25 +0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
